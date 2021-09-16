03480c9e5d2aebc95c0c93baa345d49b72d40fc7
ProjektyOA <projektyoauh@seznam.cz>
2021-09-16 08:56:06 +0200

    Přidán redaktor a vytvořen objekt redaktor Karel

---
+++ src/cvicenigit/Redaktor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cvicenigit;
+
+/**
+ *
+ * @author hodl
+ */
+public class Redaktor {
+  private String jmeno;
+
+    public Redaktor(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+  
+}
